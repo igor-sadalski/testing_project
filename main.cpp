24e0f67e665527f644e8b3113431db6bbcd7dac6
Igor Sadalski <igor.sadalski@gmail.com>
2022-07-13 14:19:53 -0700

    added some other functionalities for the project
    (cherry picked from commit cfc93f2cf8b73c080b459c4ad72f8ea28567ec90)

--- main.cpp
+++ main.cpp
@@ -5,5 +5,12 @@ int main()
     std::cout<<"Hello, World!"<<std::endl;
     //adding a new feature
     int a = 10;
+    int b = 1230123;
+    float k = 123123;
+    //adding a cherry pick commit
+    for (int i =0; i < 3; i++)
+    {
+        std::count<<"sdfsdf";
+    }
     return 0;
 }
