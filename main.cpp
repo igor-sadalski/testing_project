cfc93f2cf8b73c080b459c4ad72f8ea28567ec90
Igor Sadalski <igor.sadalski@gmail.com>
2022-07-13 14:19:53 -0700

    added some other functionalities for the project

--- main.cpp
+++ main.cpp
@@ -7,5 +7,10 @@ int main()
     int a = 10;
     int b = 1230123;
     float k = 123123;
+    //adding a cherry pick commit
+    for (int i =0; i < 3; i++)
+    {
+        std::count<<"sdfsdf";
+    }
     return 0;
 }
